6a91af8182cb5ad2160b7458587d79c946bedc21
Gelila Merid <lilabmeri1@gmail.com>
2023-08-31 23:42:50 -0400

    second commit

--- new.js
+++ new.js
@@ -34,4 +34,66 @@ function stringfy(num){
 
 console.log(update(addTwo))
 console.log(update(mutiply2))
-console.log(update(stringfy))
+
+
+
+const newArray = [];
+// loop over the array and for each array add s to the sstring and push that into the new array
+function pluralize (array) {
+  for(let i =0; i < array.length; i++) {
+    const updatedWord = array[i] + "s"
+    newArray.push(updatedWord)
+}
+  return newArray
+}
+// Uncomment these to check your work!
+const animals = ["dog", "cat", "tree frog"];
+const empty = []
+console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+console.log(pluralize(empty))
+
+
+// ADD CODE HERE
+
+// create a  function foreach that has two parmeters an aray and a 
+function forEach(array, callback) {
+    for(let i=0; i < array.length; i++) {
+        callback(array[i])
+    }
+}
+
+
+function map(arr,call) {
+
+  let newArray = [];
+  forEach(arr, function(elem){
+    
+    newArray.push(call(elem) )
+  })
+ return newArray
+}
+// Uncomment these to check your work!
+console.log(typeof forEach); // should log: 'function'
+forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+
+
+function eitherFilter(array, callback1, callback2) {
+    // ADD CODE HERE
+    const result = []
+    for(let i=0; i< array.length; i++) {
+      if(callback1(array[i]) === 'true' || callback2(array[i]) === 'true') {
+        result.push(array[i])
+      }
+        
+  }
+    return result
+  }
+ )
+  // Uncomment these to check your work!
+  const arrOfNums = [10, 35, 105, 9];
+  const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+  const over100 = n => n > 100;
+  console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
+ 
